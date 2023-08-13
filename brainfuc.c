677f21ea482798c77d0acd876e0480ba73a862e7
Pessi <pessiv@outlook.com>
2023-08-13 14:23:33 +0300

    finished this stupid project for now

--- brainfuc.c
+++ brainfuc.c
@@ -1,17 +1,21 @@
 /* ************************************************************************** **
                 
 // date: Sat Aug 12 12:46:12 AM EEST 2023
-// name: 
+// name: Hilda 
 // file: brainfuc.c
                 
 ** ************************************************************************** */
 
 #include<string.h>
 #include<stdio.h>
+#include<stdlib.h>
 
 int	number_list[30000];
 char	string_list[30000];
 int	currentpos = 0;
+int	noticeposition_command = 0;
+int	noticeposition_loop = 0;
+int	noticeposition_loopvar = 0;
 
 void	clear_arrays() {
 	for (int i = 0; i<30000; i++) {
@@ -19,11 +23,6 @@ void	clear_arrays() {
 		string_list[i] = ' ';
 	}
 }
-
-//TODO: implement [
-//TODO: implement ]
-//TODO: implement (
-//TODO: implement )
 void increment_the_byte() {
 	number_list[currentpos] += 1;
 }
@@ -36,7 +35,6 @@ void increment_the_pointer() {
 void decrement_the_pointer() {
 	currentpos--;
 }
-//TODO: implement fn brainfuck(list instruction)
 void brainfuck(char *instructions) {
 	clear_arrays();
 	int instructionlen = strlen(instructions);	
@@ -47,7 +45,12 @@ void brainfuck(char *instructions) {
 			} else if (instructions[i] == '<') {
 				decrement_the_pointer();
 			} else if (instructions[i] == ']') {
+				if (number_list[noticeposition_loopvar] > 0) {
+					i = noticeposition_loop;
+				}
 			} else if (instructions[i] == '[') {
+				noticeposition_loop = i;
+				noticeposition_loopvar = currentpos;
 			} else if (instructions[i] == '-') {
 				decrement_the_byte();
 			} else if (instructions[i] == '+') {
@@ -57,7 +60,16 @@ void brainfuck(char *instructions) {
 			} else if (instructions[i] == '.') {
 				printf("%c",number_list[currentpos]);
 			} else if (instructions[i] == '(') {
+				noticeposition_command = currentpos;
 			} else if (instructions[i] == ')') {
+				char commandtoexe[(currentpos-noticeposition_command)+1];
+				int currommand = 0;
+				for (int i = noticeposition_command; i<currentpos; i++) {
+					commandtoexe[currommand] = number_list[i];
+					currommand++;
+				}
+				char *pcte = commandtoexe;
+				system(pcte);
 			}
 		}
 	}	
